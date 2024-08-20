d7720b9ff10fa8fb0bbc3a9f827775b7a27972a3
Marcel <marcel@adriani.com>
2024-08-20 09:58:26 +0200

    Add checksum and random generator utilities
    Introduced `FletcherCheckSum` and `RandomGenerator` classes to the GeneralUtilities namespace for checksum calculations and random data generation, respectively. Also added utility methods in `GeneralUtilities` class for byte array manipulations including endian storage and cloning.

--- GeneralUtilities/GeneralUtilities.cs
+++ GeneralUtilities/GeneralUtilities.cs
@@ -1,6 +1,60 @@
 ﻿namespace GeneralUtilities;
 
+/// <summary>
+/// Provides general utility methods.
+/// </summary>
 public static class GeneralUtilities
 {
-    
+    /// <summary>
+    /// Stores the given byte array in little-endian order, if necessary.
+    /// </summary>
+    /// <param name="arr">The byte array to be stored.</param>
+    /// <returns>A new byte array stored in little-endian order, if necessary.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the input array is null.</exception>
+    /// /// <exception cref="ArgumentOutOfRangeException">Thrown when the input array is empty.</exception>
+    public static byte[] StoreLittleEndian(byte[] arr)
+    {
+        if (arr == null)
+        {
+            throw new ArgumentNullException(nameof(arr), "The array to be stored may not be null");
+        }
+
+        if (arr.Length == 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(arr), "The array to be stored may not be empty.");
+        }
+
+        byte[] bytesToChange = CloneByteArray(arr);
+
+        if (!BitConverter.IsLittleEndian)
+        {
+            Array.Reverse(bytesToChange);
+        }
+        return bytesToChange;
+    }
+
+    /// <summary>
+    /// Creates a deep copy of the given byte array.
+    /// </summary>
+    /// <param name="source">The byte array to be copied.</param>
+    /// <returns>A new byte array that is an exact copy of the given array.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the input array is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the input array is empty.</exception>
+    private static byte[] CloneByteArray(byte[] source)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source), "The array to be cloned may not be null;");
+        }
+
+        if (source.Length == 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(source), "The array to be cloned may not be empty.");
+        }
+        byte[] clone = new byte[source.Length];
+        Buffer.BlockCopy(source, 0, clone, 0, source.Length);
+        return clone;
+    }
+
+
 }
---
+++ GeneralUtilities/RandomGenerator.cs
@@ -0,0 +1,127 @@
+﻿using System.Text;
+
+namespace GeneralUtilities;
+
+/// <summary>
+/// Represents a class that generates random numbers and strings.
+/// </summary>
+public class RandomGenerator
+{
+    private readonly object _lock = new();
+
+    /// <summary>
+    /// Represents a random number generator.
+    /// </summary>
+    private readonly Random _random = new Random();
+
+    /// <summary>
+    /// The length of the generated random string in lower case.
+    /// </summary>
+    private const int LowerCaseRandomStringLength = 4;
+
+    /// <summary>
+    /// The minimum value for generating random integer numbers.
+    /// </summary>
+    private const int RandomIntNumberMin = 1000;
+
+    /// <summary>
+    /// The maximum value for generating random integer numbers.
+    /// </summary>
+    private const int RandomIntNumberMax = 9999;
+
+    /// <summary>
+    /// The constant that defines the length of the upper case string in the random password.
+    /// </summary>
+    private const int UpperCaseRandomStringLength = 2;
+
+    /// <summary>
+    /// The maximum letters offset used for generating random strings.
+    /// </summary>
+    private const int MaxLettersOffset = 26;
+
+    /// <summary>
+    /// Generates a random integer number between the specified minimum and maximum values (exclusive).
+    /// </summary>
+    /// <param name="min">The minimum value (exclusive).</param>
+    /// <param name="max">The maximum value (exclusive).</param>
+    /// <returns>A random integer number between the specified minimum and maximum values (exclusive).</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when min is not less than max.</exception>
+    public int RandomIntNumber(int min, int max)
+    {
+        if (min >= max)
+        {
+            throw new ArgumentOutOfRangeException(nameof(min), "max has to be greater than min.");
+        }
+
+        lock (_lock)
+        {
+            return _random.Next(min, max);    
+        }
+        
+    }
+
+    /// <summary>
+    /// Generates a random lowercase string of the specified size.
+    /// </summary>
+    /// <param name="size">The size of the random string to generate.</param>
+    /// <returns>A random lowercase string of the specified size.</returns>
+    public string RandomLowerCaseString(int size)
+    {
+        return GenerateRandomString(size, 'a');
+    }
+
+    /// <summary>
+    /// Generates a random string consisting of uppercase letters.
+    /// </summary>
+    /// <param name="size">The length of the generated string.</param>
+    /// <returns>A string consisting of uppercase letters.</returns>
+    public string RandomUpperCaseString(int size)
+    {
+        return GenerateRandomString(size, 'A');
+    }
+
+    /// <summary>
+    /// Generates a random string of the specified size using the given offset.
+    /// </summary>
+    /// <param name="size">The size of the random string to generate.</param>
+    /// <param name="offset">The starting character offset for generating the random string.</param>
+    /// <returns>A string containing the randomly generated characters.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when offset + MaxLettersOffset are more than Char.MaxValue</exception>
+    private string GenerateRandomString(int size, char offset)
+    {
+        if (offset > Char.MaxValue - MaxLettersOffset)
+        {
+            throw new ArgumentOutOfRangeException(nameof(offset),
+                $"offset must be between 0 and {Char.MaxValue - MaxLettersOffset}.");
+        }
+        var builder = new StringBuilder(size);
+        
+        for (var i = 0; i < size; i++)
+        {
+            char @char;
+            lock (_lock)
+            {
+                @char = (char)_random.Next(offset, offset + MaxLettersOffset);
+            }
+            builder.Append(@char);
+        }   
+        return builder.ToString();
+    }
+
+    /// <summary>
+    /// Generates a random password consisting of lowercase letters, numbers, and uppercase letters.
+    /// </summary>
+    /// <returns>A randomly generated password.</returns>
+    public string RandomPassword()
+    {
+        var passwordBuilder = new StringBuilder();
+        lock (_lock)
+        {
+            passwordBuilder.Append(RandomLowerCaseString(LowerCaseRandomStringLength))
+                .Append(RandomIntNumber(RandomIntNumberMin, RandomIntNumberMax))
+                .Append(RandomUpperCaseString(UpperCaseRandomStringLength));
+        }
+        
+        return passwordBuilder.ToString();
+    }  
+}
