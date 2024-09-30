11f8b68551cbd2dda9a7dfc0e5251a8a5a635528
Marcel <mjad1963@gmail.com>
2024-09-30 15:32:02 +0200

    - Debugged FletcherCheckSum.cs - Added unit tests FletcherCheckSumTests.cs - Added NUnint from NUget to enable testing

--- GeneralUtilities/FletcherCheckSum.cs
+++ GeneralUtilities/FletcherCheckSum.cs
@@ -11,153 +11,71 @@
 // 
 //     */
 
-using System.Buffers.Binary;
-
 namespace GeneralUtilities;
 
 /// <summary>
 /// Represents a utility class for calculating Fletcher checksums.
 /// </summary>
-public class FletcherCheckSum
+public static class FletcherCheckSum
 {
     /// <summary>
-    /// Represents the sizes of the Fletcher checksum types.
+    /// Represents the types of Fletcher checksums that can be calculated.
     /// </summary>
-    public readonly struct FletcherSizes
+    public enum FletcherTypes
     {
-        /// <summary>
-        /// Represents the type of Fletcher checksum.
-        /// </summary>
-        private FletcherTypes FletcherType { get; }
-
-        /// <summary>
-        /// Represents the available types of Fletcher checksums.
-        /// </summary>
-        public enum FletcherTypes
-        {
-            /// <summary>
-            /// Represents the Fletcher16 member of the FletcherTypes enum in the GeneralUtilities namespace.
-            /// </summary>
-            Fletcher16 = 16,
-
-            /// <summary>
-            /// Represents the Fletcher32 member of the FletcherTypes enumeration.
-            /// </summary>
-            Fletcher32 = 32,
-
-            /// <summary>
-            /// Represents the Fletcher64 member of the FletcherTypes enum.
-            /// </summary>
-            Fletcher64 = 64
-        }
-        public FletcherSizes(FletcherTypes fletcherType)
-        {
-            FletcherType = fletcherType;
-        }
-        // Converting method to property
-        /// <summary>
-        /// Gets the size of each block used for calculating the checksum.
-        /// </summary>
-        /// <remarks>
-        /// The block size is determined by the Fletcher type set in the <see cref="FletcherSizes"/> struct.
-        /// </remarks>
-        /// <returns>
-        /// The size of each block, in bytes.
-        /// </returns>
-        public int BlockSize => (int)FletcherType / 16;
-    }
-    
-    /// <summary>
-    /// Calculates the checksum from the given byte array using the specified number of bits per block.
-    /// </summary>
-    /// <param name="inputAsBytes">The byte array to calculate the checksum from.</param>
-    /// <param name="bitsPerBlock">The number of bits per block used in the Fletcher checksum algorithm.</param>
-    /// <returns>The calculated checksum as an unsigned 64-bit integer.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when the input byte array is null.</exception
-    public static UInt64 CalculateChecksumFromBytes(byte[] inputAsBytes, FletcherSizes.FletcherTypes bitsPerBlock)
-    {
-        if (inputAsBytes == null)
-        {
-            throw new ArgumentNullException(nameof(inputAsBytes),
-                "The input for calculating the checksum may nog be null.");
-        }
-
-        var fletcherType = new FletcherSizes(bitsPerBlock);
-        
-        UInt64 maxBlockValue = (UInt64)(Math.Pow(2, 8 * fletcherType.BlockSize) - 1);
-        UInt64 sum1 = 0;
-        UInt64 sum2 = 0;
-        try
-        {
-            foreach (UInt64 block in ChunkIntoBlocks(inputAsBytes, fletcherType))
-            {
-                sum1 = checked((sum1 + block) % maxBlockValue);
-                sum2 = checked((sum2 + sum1) % maxBlockValue);
-            }
-            return checked(sum1 + (sum2 * (maxBlockValue + 1)));
-        }
-        catch (OverflowException exception)
-        {
-            throw;
-        }
-        
-        
+        Fletcher16 = 16,
+        Fletcher32 = 32,
+        Fletcher64 = 64
     }
 
     /// <summary>
-    /// Splits the given byte array into blocks of a specified size.
+    /// Calculates the Fletcher checksum from an array of bytes.
     /// </summary>
-    /// <param name="inputAsBytes">The byte array to be split into blocks.</param>
-    /// <param name="fletcherType">The type of Fletcher checksum used to calculate the checksum.</param>
-    /// <returns>An IEnumerable collection of UInt64 representing the calculated blocks.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when the input array is null.</exception>
-    /// <exception cref="ArgumentOutOfRangeException">Thrown when the fletcherType is invalid.</exception>
-    private static IEnumerable<UInt64> ChunkIntoBlocks(byte[] inputAsBytes, FletcherSizes fletcherType)
+    /// <param name="input">The array of bytes for which the checksum will be calculated.</param>
+    /// <param name="bitsPerBlock">A member of enum FletcherTypes, representing the type of Fletcher calculation</param>
+    /// <returns>The calculated Fletcher checksum as an unsigned 64-bit integer.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when input is null</exception>
+    public static UInt64 CalculateChecksumFromBytes(IReadOnlyList<byte> input, FletcherTypes bitsPerBlock)
     {
-        if (inputAsBytes == null)
-        {
-            throw new ArgumentNullException(nameof(inputAsBytes),
-                "The input for calculating the checksum may nog be null.");
-        }
-        
-        var blockSize = fletcherType.BlockSize;
-        if (blockSize != (int)FletcherSizes.FletcherTypes.Fletcher16 &&
-            blockSize != (int)FletcherSizes.FletcherTypes.Fletcher32 &&
-            blockSize != (int)FletcherSizes.FletcherTypes.Fletcher64)
-        {
-            throw new ArgumentOutOfRangeException(nameof(fletcherType));
-        }
-        
-        // Padding byte array to be divisible by blockSize with 0s
-        int length = ((inputAsBytes.Length + blockSize - 1) / blockSize) * blockSize;
-        byte[] bytes = new byte[length];
-        inputAsBytes.CopyTo(bytes, 0);
-        Array.Clear(bytes, inputAsBytes.Length, length - inputAsBytes.Length);
+        if (input == null)
+            throw new ArgumentNullException(nameof(input), "Array for calculating checksum may not be null,");
+            
+        var bytesPerCycle = (int) bitsPerBlock / 16;
+        var modValue = (ulong) (Math.Pow(2, 8 * bytesPerCycle) - 1);
 
-        for (int i = 0; i < length; i += blockSize)
+        ulong sum1 = 0;
+        ulong sum2 = 0;
+
+        foreach (var block in Blockify(input, bytesPerCycle))
         {
-            yield return CalculateBlockFromBytes(bytes, i, blockSize);
+            sum1 = (sum1 + block) % modValue;
+            sum2 = (sum2 + sum1) % modValue;
         }
+
+        return sum1 + sum2 * (modValue + 1);
     }
 
     /// <summary>
-    /// Calculates a block of data from a byte array.
+    /// Splits the given input byte array into blocks of the specified size and calculates the Fletcher checksum.
     /// </summary>
-    /// <param name="bytes">The byte array containing the data.</param>
-    /// <param name="index">The starting index of the block.</param>
-    /// <param name="blockSize">The size of the block.</param>
-    /// <returns>The calculated block as an unsigned 64-bit integer.</returns>
-    private static UInt64 CalculateBlockFromBytes(byte[] bytes, int index, int blockSize)
+    /// <param name="inputAsBytes">The input byte array.</param>
+    /// <param name="blockSize">The number of blocks (1, 2 or 4) in a group</param>
+    /// <returns>An IEnumerable&lt;ulong&gt; of the carved out blocks</returns>
+    private static IEnumerable<ulong> Blockify(IReadOnlyList<byte> inputAsBytes, int blockSize)
     {
-        if (blockSize == (int)FletcherSizes.FletcherTypes.Fletcher16)
-            return BinaryPrimitives.ReadUInt16LittleEndian(bytes.AsSpan(index));
-        if (blockSize == (int)FletcherSizes.FletcherTypes.Fletcher32)
-            return BinaryPrimitives.ReadUInt32LittleEndian(bytes.AsSpan(index));
-        if (blockSize == (int)FletcherSizes.FletcherTypes.Fletcher64)
-            return BinaryPrimitives.ReadUInt64LittleEndian(bytes.AsSpan(index));
+        var i = 0;
+        ulong block = 0;
 
-        return 0;
+        while (i < inputAsBytes.Count)
+        {
+            block = (block << 8) | inputAsBytes[i];
+            i++;
+
+            if (i % blockSize != 0 && i != inputAsBytes.Count) continue;
+
+            yield return block;
+            block = 0;
+        }
     }
-
 
 }
---
+++ gx000touchpadUnitTests/GeneralUtilities/FletcherCheckSumTests.cs
@@ -0,0 +1,75 @@
+﻿// //     * Copyright (c) 2024 - 2024 Marcel Adriani
+// //     *
+// //     * This file is part of gx000touchpad.
+// //
+// //      * gx000touchpad is free software: you can redistribute it and/or modify it under the terms of the
+// //          GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
+// //          or (at your option) any later version.
+// //
+// //     * gx000touchpad is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// //          without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// //          See the GNU General Public License for more details.
+// //
+// //     * You should have received a copy of the GNU General Public License along with Foobar.
+// //          If not, see <https://www.gnu.org/licenses/>.
+
+using System.Text;
+using GeneralUtilities;
+
+namespace gx000touchpadUnitTests.GeneralUtilities;
+
+[TestFixture]
+public class FletcherCheckSumTests
+{
+    //TestVectors
+    private const string Test1 = "abcde";
+    private const string Test2 = "abcdef";
+    private const string Test3 = "abcdefgh";
+
+    private const UInt64 Result16Bit1 = 51440;
+    private const UInt64 Result16Bit2 = 8279;
+    private const UInt64 Result16Bit3 = 1575;
+    
+    private const UInt64 Result32Bit1 = 3948201259;
+    private const UInt64 Result32Bit2 = 1347824173;
+    private const UInt64 Result32Bit3 = 3790311829;
+    
+    private const UInt64 Result64Bit1 = 14034561336514601929;
+    private const UInt64 Result64Bit2 = 14034672391484000458;
+    private const UInt64 Result64Bit3 = 2894457976838998732;
+
+    private const FletcherCheckSum.FletcherTypes Fletcher16 = FletcherCheckSum.FletcherTypes.Fletcher16;
+    private const FletcherCheckSum.FletcherTypes Fletcher32 = FletcherCheckSum.FletcherTypes.Fletcher32;
+    private const FletcherCheckSum.FletcherTypes Fletcher64 = FletcherCheckSum.FletcherTypes.Fletcher64;
+    
+    [Test]
+    [TestCase(Test1,Fletcher16,Result16Bit1)]
+    [TestCase(Test2, Fletcher16, Result16Bit2)]
+    [TestCase(Test3, Fletcher16, Result16Bit3)]
+    [TestCase(Test1, Fletcher32, Result32Bit1)]
+    [TestCase(Test2, Fletcher32, Result32Bit2)]
+    [TestCase(Test3, Fletcher32, Result32Bit3)]
+    [TestCase(Test1, Fletcher64, Result64Bit1)]
+    [TestCase(Test2, Fletcher64, Result64Bit2)]
+    [TestCase(Test3, Fletcher64, Result64Bit3)]
+    public void CalculateChecksumFromBytes_TestVectors_ReturnResult(
+        string inputString,FletcherCheckSum.FletcherTypes fletcherType, UInt64 expectedResult)
+    {
+        byte[] inputArray = Encoding.UTF8.GetBytes(inputString);
+
+        var result = FletcherCheckSum.CalculateChecksumFromBytes(inputArray, fletcherType);
+
+        Assert.That(result == expectedResult);
+    }
+    
+    [Test]
+    public void CalculateChecksumFromBytes_ArrayIsNull_ThrowException()
+    {
+        byte[] inputArray = null;
+
+        Assert.That(() => FletcherCheckSum.CalculateChecksumFromBytes(inputArray, Fletcher64),
+            Throws.InstanceOf<ArgumentNullException>());
+       
+    }
+    
+}
