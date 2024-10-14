0462c63d8f555d61bbb52f53cff7a9ddfa51a7cd
Marcel <mjad1963@gmail.com>
2024-10-14 14:30:33 +0200

    Add unit tests for StringVariable class
    Introduced new unit tests for the StringVariable class to verify getter and setter functionalities. Also, created a TestableStringVariable class to test status change behavior.

---
+++ gx000touchpadUnitTests/gx000data/StringVariableTests.cs
@@ -0,0 +1,84 @@
+﻿// //     * Copyright (c) 2024 - 2024 Marcel Adriani
+// //     *
+// //     * This file is part of gx000touchpad.
+// //
+// //      * gx000touchpad is free software: you can redistribute it and/or modify it under the terms of the
+// //          GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
+// //          or (at your option) any later version.
+// //
+// //     * gx000touchpad is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// //          without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// //          See the GNU General Public License for more details.
+// //
+// //     * You should have received a copy of the GNU General Public License along with Foobar.
+// //          If not, see <https://www.gnu.org/licenses/>.
+
+using gx000data;
+
+namespace gx000touchpadUnitTests.gx000data;
+
+[TestFixture]
+public class StringVariableTests
+{
+    private StringDataConverter _converter;
+    private TestableStringVariable _variable;
+    private string _testValue;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _testValue = "AbCdEfGhIj";
+        _converter = new StringDataConverter();
+        _variable = new TestableStringVariable(
+            VariableDefinitions.FirstMessageName,
+            DataExchange.DataStatus.Synchronized,
+            _testValue);
+    }
+    
+    [Test]
+    public void TypeVariableValueGetter_WhenCalled_ShouldReturnCorrectValue()
+    {
+        // Act
+        var actualValue = _variable.Value;
+
+        // Assert
+        Assert.That(actualValue, Is.EqualTo(_testValue));
+    }
+    
+    [Test]
+    public void TypeVariableValueSetter_WhenCalled_ShouldReturnCorrectValue()
+    {
+        // Act
+        var testValue = "KlMnOpQrSt";
+        _variable.Value = testValue;
+        var actualValue = _variable.Value;
+
+        // Assert
+        Assert.That(actualValue, Is.EqualTo(testValue));
+    }
+    
+    [Test]
+    public void VariableStatusSetter_StatusChangeIsOkIsFalse_ShouldThrowInvalidOperationException()
+    {
+        Assert.That(() => _variable.Status = DataExchange.DataStatus.Test, 
+            Throws.TypeOf<InvalidOperationException>());
+    }
+
+    [Test]
+    public void VariableStatusSetter_ChangeIsValid_NewStatusIsSet()
+    {
+        _variable.Status = DataExchange.DataStatus.FromClientToSim;
+        
+        Assert.That(_variable.Status == DataExchange.DataStatus.FromClientToSim);
+    }
+    
+    [Test]
+    public void VariableStatusSetter_ChangeIsValid_OnStatusChangedIsCalled()
+    {
+        _variable.Status = DataExchange.DataStatus.FromClientToSim;
+        
+        Assert.IsTrue(_variable.OnStatusChangedCalled);
+    }
+
+
+}
---
+++ gx000touchpadUnitTests/gx000data/TestableStringVariable.cs
@@ -0,0 +1,34 @@
+﻿// //     * Copyright (c) 2024 - 2024 Marcel Adriani
+// //     *
+// //     * This file is part of gx000touchpad.
+// //
+// //      * gx000touchpad is free software: you can redistribute it and/or modify it under the terms of the
+// //          GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
+// //          or (at your option) any later version.
+// //
+// //     * gx000touchpad is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// //          without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// //          See the GNU General Public License for more details.
+// //
+// //     * You should have received a copy of the GNU General Public License along with Foobar.
+// //          If not, see <https://www.gnu.org/licenses/>.
+
+using gx000data;
+
+namespace gx000touchpadUnitTests.gx000data;
+
+public class TestableStringVariable : StringVariable
+{
+    public TestableStringVariable(string variableName, DataExchange.DataStatus dataStatus, string dataValue)
+        : base(variableName, dataStatus, dataValue)
+    {
+        
+    }
+    public bool OnStatusChangedCalled { get; private set; }
+
+    protected override void OnStatusChanged()
+    {
+        OnStatusChangedCalled = true;
+        base.OnStatusChanged();
+    }
+}
