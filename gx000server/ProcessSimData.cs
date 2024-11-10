07ab4a714640d6c6e1159058fc295384e69a92bf
Marcel <mjad1963@gmail.com>
2024-11-10 15:43:31 +0100

    Update ProcessSimData to implement IDisposable and add documentation
    Refactored ProcessSimData to implement the IDisposable interface for proper resource management. Added extensive XML documentation comments to clarify the purpose and functionality of each member within the class.
    
    Signed-off-by: Marcel <mjad1963@gmail.com>

--- gx000server/ProcessSimData.cs
+++ gx000server/ProcessSimData.cs
@@ -2,19 +2,83 @@
 using System.Globalization;
 using System.Reactive.Linq;
 using System.Text.RegularExpressions;
+using GeneralUtilities;
 using gx000data;
 
 namespace gx000server;
 
-public class ProcessSimData : INotifyPropertyChanged
+/// <summary>
+/// Handles the processing of simulation data for flight simulation content.
+/// </summary>
+public class ProcessSimData : INotifyPropertyChanged, IDisposable
 {
+    /// <summary>
+    /// Holds an instance of the GenerateFlightSimContent class, which generates
+    /// variable content for simulation/testing purposes.
+    /// </summary>
     private readonly GenerateFlightSimContent _content;
+
+    /// <summary>
+    /// Represents an active subscription to observable events generated from property changes
+    /// in the GenerateFlightSimContent instance.
+    /// The subscription listens for PropertyChanged events and processes them accordingly.
+    /// </summary>
     private readonly IDisposable _subscription;
+
+    /// <summary>
+    /// Holds the name of a variable being processed for simulation purposes.
+    /// </summary>
     private string _variableName;
+
+    /// <summary>
+    /// Represents the data type associated with the current variable in the ProcessSimData class.
+    /// This field is private and is used internally within the ProcessSimData class to manage
+    /// the type of data being processed.
+    /// </summary>
     private string _dataType;
+
+    /// <summary>
+    /// Holds the data of the currently active variable within the simulation.
+    /// </summary>
     private Variable _currentVariable;
-    
+
+    /// <summary>
+    /// Event that is triggered when a property value changes.
+    /// Implements the INotifyPropertyChanged interface's PropertyChanged event.
+    /// </summary>
     public event PropertyChangedEventHandler? PropertyChanged;
+    /// <summary>
+    /// The ProcessSimData class is responsible for processing simulated flight data. It listens for property changes
+    /// in the GenerateFlightSimContent and processes them accordingly. The class implements the INotifyPropertyChanged
+    /// interface to notify the UI of any updates to its properties.
+    /// </summary>
+    /// <remarks>
+    /// The class subscribes to property change events from the GenerateFlightSimContent and updates the CurrentVariable
+    /// property based on the new values. It processes different data types such as StringType, IntType, and LongType
+    /// to create appropriate Variable objects.
+    /// </remarks>
+    /// <example>
+    /// ProcessSimData is instantiated by passing a GenerateFlightSimContent object to its constructor. The class listens
+    /// for changes and processes them automatically.
+    /// </example>
+    /// <seealso cref="GenerateFlightSimContent"/>
+    /// <seealso cref="Variable"/>
+    public ProcessSimData(GenerateFlightSimContent content)
+    {
+        _content = content;
+        
+        _subscription = Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+                h => content.PropertyChanged += h,
+                h => content.PropertyChanged -= h)
+            .Subscribe(e => OnNext(e.EventArgs));
+    }
+    
+    /// <summary>
+    /// Gets or sets the current <see cref="Variable"/> instance.
+    /// </summary>
+    /// <remarks>
+    /// This property represents the variable in the simulation data processing context currently being processed.
+    /// </remarks>
     public Variable CurrentVariable
     {
         get => _currentVariable;
@@ -25,6 +89,12 @@ public class ProcessSimData : INotifyPropertyChanged
         }
     }
 
+    /// <summary>
+    /// Gets or sets the name of the variable generated from the simulation content.
+    /// </summary>
+    /// <remarks>
+    /// The value of this property is set whenever a property change is detected in the GenerateFlightSimContent instance.
+    /// </remarks>
     public string VariableName
     {
         get => _variableName;
@@ -36,6 +106,15 @@ public class ProcessSimData : INotifyPropertyChanged
         }
     }
 
+    /// <summary>
+    /// Gets the type of the data associated with the current variable.
+    /// The property value is derived from the variable attributes fetched
+    /// from the VariableDefinitions class based on the variable name.
+    /// </summary>
+    /// <value>
+    /// The type of the data, such as "StringType", "IntType", or "LongType".
+    /// Changes to this property trigger the PropertyChanged event.
+    /// </value>
     public string DataType
     {
         get => _dataType;
@@ -47,25 +126,52 @@ public class ProcessSimData : INotifyPropertyChanged
         }
     }
 
-    public ProcessSimData(GenerateFlightSimContent content)
-    {
-        _content = content;
-        
-        _subscription = Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
-                h => content.PropertyChanged += h,
-                h => content.PropertyChanged -= h)
-            .Subscribe(e => OnNext(e.EventArgs));
-    }
-
-    protected virtual void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+   
+    /// <summary>
+    /// Raises the PropertyChanged event.
+    /// </summary>
+    /// <param name="propertyName">
+    /// The name of the property that changed. This is optional and can be supplied automatically
+    /// by the CallerMemberName attribute.
+    /// </param>
+    protected virtual void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
     }
+
+    /// <summary>
+    /// Releases the resources used by the ProcessSimData instance.
+    /// </summary>
+    /// <remarks>
+    /// This method is responsible for cleaning up resources consumed by the ProcessSimData object.
+    /// It invokes the Dispose method with disposing set to true to release managed resources and
+    /// then calls SuppressFinalize to prevent finalization.
+    /// </remarks>
+    /// <seealso cref="System.IDisposable"/>
     public void Dispose()
     {
-        _subscription.Dispose();
+        Dispose(true);
+        GC.SuppressFinalize(this);
     }
+
+    /// <summary>
+    /// Releases all resources used by the ProcessSimData instance.
+    /// </summary>
+    /// <param name="disposing">Indicates whether the method call comes from a Dispose method (its value is true)
+    /// or from a finalizer (its value is false).</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _subscription.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// Handles the event when a property value changes in the observed GenerateFlightSimContent instance.
+    /// Updates the appropriate fields and processes the new value based on its data type.
+    /// </summary>
+    /// <param name="e">The event arguments containing the name of the property that changed.</param>
     private void OnNext(PropertyChangedEventArgs e)
     {
         VariableName = e.PropertyName;
@@ -75,9 +181,11 @@ public class ProcessSimData : INotifyPropertyChanged
             throw new InvalidOperationException($"{VariableName} is not a valid property name");
 
         GetType(VariableName);
-        var contentValue = (string)propertyInfo.GetValue(_content);
-        var numberContentValue = UnformatNumber(contentValue);
         
+        var contentValue = propertyInfo.GetValue(_content) as string
+            ?? throw new InvalidOperationException($"{VariableName} does not contain a string value.");
+        
+        var numberContentValue = NumberServices.UnformatNumber(contentValue);
         
         switch (DataType)
         {
@@ -111,41 +219,22 @@ public class ProcessSimData : INotifyPropertyChanged
                 }
                 break;
         }
-        
-
-        //Console.WriteLine($"{e.PropertyName} is gewijzigd. Type is: {DataType}.");
     }
 
-    private void CheckType(Type propertyType, Type definedType)
-    {
-        Console.WriteLine($"PropertyType = {propertyType.Name}, DefinedType = {definedType.Name}.");
-        if (propertyType.Name != definedType.Name)
-            throw new InvalidOperationException($"{definedType.FullName} is not a valid type");
-    }
-
+    /// <summary>
+    /// Retrieves the data type of a variable based on its name and sets the DataType property accordingly.
+    /// </summary>
+    /// <param name="variableName">The name of the variable whose data type is to be determined.</param>
+    /// <exception cref="Exception">Thrown when the variable is not supported.</exception>
     private void GetType(string variableName)
     {
         try
         {
             DataType = VariableDefinitions.GetVariableAttributes(variableName).Type;
         }
-        catch
+        catch (KeyNotFoundException e)
         {
-            throw new Exception($"Variable {variableName} is not supported");
+            throw new Exception($"Variable {variableName} is not supported", e);
         }
     }
-    private static string UnformatNumber(string formattedNumber)
-    {
-        var cultureInfo = CultureInfo.CurrentCulture;
-        
-        char decimalSeparator = cultureInfo.NumberFormat.NumberDecimalSeparator[0];
-        char groupSeparator = cultureInfo.NumberFormat.NumberGroupSeparator[0];
-
-        string result = formattedNumber.Replace(groupSeparator.ToString(), string.Empty);
-
-        result = result.Replace(decimalSeparator, '.');
-        
-        return Regex.Replace(result, @"[^0-9.-]", "");
-    }
-
 }
